37e87f0a456d0f2c2700c75efde2e6d306e2f73a
GabrielLawisch <gabriellawisch2005@gmail.com>
2022-05-25 16:08:59 -0300

    InnitialCommit

--- Lista5.java/ex4List5.java
+++ Lista5.java/ex4List5.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
-public class ex3List5 {
+public class ex4List5 {
   public static void main(String[] args) {
     Scanner entrada = new Scanner(System.in);
-
-    for(int n = 70; n >= 25; n--){
+    for(int n = 95; n >= 25; n -= 2){
         System.out.println(n);
   }    
-    entrada.close();
-  }
+  entrada.close();
+ }
 }
