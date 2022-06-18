f21a624a603e47125167434418f951020b46d551
GabrielLawisch <gabriellawisch2005@gmail.com>
2022-06-17 23:52:16 -0300

    initialCommit

---
+++ matriz.java/jogoDaVelha.java
@@ -0,0 +1,14 @@
+/*
+Usando uma matriz 3x3 de caracteres crie um jogo da velha. 
+O jogo pode ser jogado por um ou dois jogadores.
+O jogador número 1 usa o X e o jogador 2 é representado pelo O. 
+O programa deve sortear quem inicia o jogo. O jogador deve indicar a linha e a coluna que quer jogar. Se a casa já estiver ocupada o jogador deve selecionar outra casa. Ganha quem 
+fizer uma sequência de símbolos iguais em uma linha, coluna ou diagonal.
+
+*/
+
+class jogoDaVelha {
+    public static void main(String[] args) {
+      System.out.println("Hello world!");
+    }
+  }
